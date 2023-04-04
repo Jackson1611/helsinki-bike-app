818ac34aab561085780e41c46b900141185765a4
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-04-04 14:44:51 +0300

    page pagination made

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,7 +5,7 @@ function App() {
   const [data, setData] = useState([]);
   const [page, setPage] = useState(1);
   const [totalRows, setTotalRows] = useState(1);
-  const pageSize = 20;
+  const pageSize = 17;
 
   useEffect(() => {
     async function fetchData() {
@@ -28,43 +28,66 @@ function App() {
     {
       field: "departure_station_id",
       headerName: "Departure Station ID",
-      width: 200,
+      width: 150,
     },
     {
       field: "departure_station_name",
       headerName: "Departure Station Name",
-      width: 300,
+      width: 200,
     },
     { field: "return_station_id", headerName: "Return Station ID", width: 200 },
     {
       field: "return_station_name",
       headerName: "Return Station Name",
-      width: 300,
+      width: 200,
     },
     { field: "covered_distance", headerName: "Covered Distance", width: 200 },
     { field: "duration", headerName: "Duration", width: 150 },
   ];
 
   return (
-    <div>
+    <div style={{ display: "flex", height: "100vh" }}>
+      <div style={{ backgroundColor: "#f2f2f2", width: "20%" }}>
+        {/* Navigation bar content here */}
+      </div>
       <div
         style={{
+          width: "80%",
           display: "flex",
-          height: 1000,
-          width: "100%",
-          display: "flex",
-          justifyContent: "center",
+          flexDirection: "column",
           alignItems: "center",
+          justifyContent: "center",
         }}
       >
-        <DataGrid
-          rows={data}
-          pagination
-          columns={columns}
-          pageSize={pageSize}
-          onPageChange={(params) => setPage(params.page)}
-          rowCount={totalRows}
-        />
+        <div style={{ width: "80%", height: "1000px" }}>
+          <DataGrid
+            rows={data}
+            pagination
+            columns={columns}
+            pageSize={pageSize}
+            rowCount={totalRows}
+            onPageChange={(newPage) => setPage(newPage)}
+            hideFooterPagination
+          />
+        </div>
+        <div
+          style={{ display: "flex", justifyContent: "center", marginTop: 20 }}
+        >
+          <button
+            disabled={page === 1}
+            onClick={() => setPage(page - 1)}
+            style={{ fontSize: 16, padding: 10 }}
+          >
+            Previous Page
+          </button>
+          <button
+            disabled={page * pageSize >= totalRows}
+            onClick={() => setPage(page + 1)}
+            style={{ fontSize: 16, padding: 10, marginLeft: 10 }}
+          >
+            Next Page
+          </button>
+        </div>
       </div>
     </div>
   );
