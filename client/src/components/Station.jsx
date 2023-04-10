baa3018dba84e026006dc28c6572a0154752ba84
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-04-10 12:06:51 +0300

    added search bar for station view

--- client/src/components/Station.jsx
+++ client/src/components/Station.jsx
@@ -1,9 +1,14 @@
-import { useEffect, useState } from "react";
-import { DataGrid, GridToolbar } from "@mui/x-data-grid";
+import React, { useEffect, useState } from "react";
+import {
+  DataGrid,
+  GridToolbar,
+  GridToolbarQuickFilter,
+  GridLogicOperator,
+} from "@mui/x-data-grid";
+import Box from "@mui/material/Box";
 
 export function Station() {
   const [stations, setStations] = useState([]);
-  const [filteredStations, setFilteredStations] = useState([]);
   const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
@@ -16,6 +21,28 @@ export function Station() {
       .catch((error) => console.error(error));
   }, []);
 
+  function QuickSearchToolbar() {
+    return (
+      <Box
+        sx={{
+          p: 0.5,
+          pb: 0,
+          display: "flex",
+          justifyContent: "flex-end",
+          padding: "10px",
+        }}
+      >
+        <GridToolbarQuickFilter
+          quickFilterParser={(searchInput) =>
+            searchInput
+              .split(",")
+              .map((value) => value.trim())
+              .filter((value) => value !== "")
+          }
+        />
+      </Box>
+    );
+  }
   const columns = [
     { field: "id", headerName: "ID", width: 100 },
     { field: "fi_name", headerName: "Finnish Name", width: 200 },
@@ -34,7 +61,17 @@ export function Station() {
   return (
     <div style={{ height: 1000, width: "100%" }}>
       <h1>Stations</h1>
-      <DataGrid rows={filteredStations} columns={columns} />
+      <DataGrid
+        slots={{
+          toolbar: () => (
+            <React.Fragment>
+              <QuickSearchToolbar />
+            </React.Fragment>
+          ),
+        }}
+        rows={stations}
+        columns={columns}
+      />
     </div>
   );
 }
