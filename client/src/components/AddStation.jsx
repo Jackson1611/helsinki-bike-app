45c8ac16f5095cc04e0890b19aad3f804005a417
Nguyen Huu Loc <103312138+Jackson1611@users.noreply.github.com>
2023-04-13 15:54:27 +0300

    implemented input validation for creating new station

--- client/src/components/AddStation.jsx
+++ client/src/components/AddStation.jsx
@@ -21,10 +21,11 @@ export function AddStation(props) {
     se_city: "",
     operator_name: "",
     capacity: "",
-    longitude: 24.93545, // Default longitude for Helsinki
-    latitude: 60.16952, // Default latitude for Helsinki
+    longitude: 24.93545,
+    latitude: 60.16952,
   });
   const [success, setSuccess] = useState(false);
+  const [errors, setErrors] = useState({});
 
   const handleClickOpen = () => {
     setOpen(true);
@@ -32,14 +33,7 @@ export function AddStation(props) {
 
   const handleClose = () => {
     setOpen(false);
-  };
-
-  const handleInputChange = (e) => {
-    setNewStation({ ...newStation, [e.target.name]: e.target.value });
-  };
-
-  const addStation = () => {
-    props.saveStation(newStation);
+    setErrors({});
     setNewStation({
       fi_name: "",
       se_name: "",
@@ -50,13 +44,68 @@ export function AddStation(props) {
       se_city: "",
       operator_name: "",
       capacity: "",
-      longitude: "",
-      latitude: "",
+      longitude: 24.93545,
+      latitude: 60.16952,
     });
-    handleClose();
-    setSuccess(true);
   };
 
+  const handleInputChange = (e) => {
+    setNewStation({ ...newStation, [e.target.name]: e.target.value });
+  };
+
+  const addStation = () => {
+    const validationErrors = validateInputs(newStation);
+    if (Object.keys(validationErrors).length === 0) {
+      props.saveStation(newStation);
+      setNewStation({
+        fi_name: "",
+        se_name: "",
+        en_name: "",
+        fi_address: "",
+        se_address: "",
+        fi_city: "",
+        se_city: "",
+        operator_name: "",
+        capacity: "",
+        longitude: "",
+        latitude: "",
+      });
+      handleClose();
+      setSuccess(true);
+    } else {
+      setErrors(validationErrors);
+    }
+  };
+
+  // Function to validate input fields
+  const validateInputs = (inputs) => {
+    const errors = {};
+    const requiredFields = [
+      "fi_name",
+      "se_name",
+      "en_name",
+      "fi_address",
+      "se_address",
+      "fi_city",
+      "se_city",
+      "operator_name",
+      "capacity",
+    ];
+
+    // Check for required fields
+    requiredFields.forEach((field) => {
+      if (!inputs[field]) {
+        errors[field] = "This field is required";
+      }
+    });
+
+    // Check for numeric value in capacity field
+    if (inputs.capacity && isNaN(inputs.capacity)) {
+      errors.capacity = "Please enter a valid number";
+    }
+
+    return errors;
+  };
   const handleMarkerDragEnd = (e) => {
     setNewStation({
       ...newStation,
@@ -88,76 +137,94 @@ export function AddStation(props) {
                 onChange={(event) => handleInputChange(event)}
                 label="Finnish name"
                 fullWidth
+                error={errors.fi_name}
+                helperText={errors.fi_name}
               />
               <TextField
                 margin="dense"
                 name="se_name"
                 value={newStation.se_name}
-                onChange={(event) => handleInputChange(event)}
-                label="Swedish name"
+                label="Name (Swedish)"
                 fullWidth
+                onChange={handleInputChange}
+                error={errors.se_name}
+                helperText={errors.se_name}
               />
               <TextField
                 margin="dense"
                 name="en_name"
                 value={newStation.en_name}
-                onChange={(event) => handleInputChange(event)}
-                label="English name"
+                label="Name (English)"
                 fullWidth
+                onChange={handleInputChange}
+                error={errors.en_name}
+                helperText={errors.en_name}
               />
               <TextField
                 margin="dense"
                 name="fi_address"
                 value={newStation.fi_address}
-                onChange={(event) => handleInputChange(event)}
-                label="Finish address"
+                label="Address (Finnish)"
                 fullWidth
+                onChange={handleInputChange}
+                error={errors.fi_address}
+                helperText={errors.fi_address}
               />
               <TextField
                 margin="dense"
                 name="se_address"
                 value={newStation.se_address}
-                onChange={(event) => handleInputChange(event)}
-                label="Swedish address"
+                label="Address (Swedish)"
                 fullWidth
+                onChange={handleInputChange}
+                error={errors.se_address}
+                helperText={errors.se_address}
               />
               <TextField
                 margin="dense"
                 name="fi_city"
                 value={newStation.fi_city}
-                onChange={(event) => handleInputChange(event)}
-                label="Finnish city"
+                label="City (Finnish)"
                 fullWidth
+                onChange={handleInputChange}
+                error={errors.fi_city}
+                helperText={errors.fi_city}
               />
               <TextField
                 margin="dense"
                 name="se_city"
                 value={newStation.se_city}
-                onChange={(event) => handleInputChange(event)}
-                label="Swedish city"
+                label="City (Swedish)"
                 fullWidth
+                onChange={handleInputChange}
+                error={errors.se_city}
+                helperText={errors.se_city}
               />
               <TextField
                 margin="dense"
                 name="operator_name"
                 value={newStation.operator_name}
-                onChange={(event) => handleInputChange(event)}
-                label="Operator name"
+                label="Operator"
                 fullWidth
+                onChange={handleInputChange}
+                error={errors.operator_name}
+                helperText={errors.operator_name}
               />
               <TextField
                 margin="dense"
                 name="capacity"
                 value={newStation.capacity}
-                onChange={(event) => handleInputChange(event)}
                 label="Capacity"
                 fullWidth
+                onChange={handleInputChange}
+                error={errors.capacity}
+                helperText={errors.capacity}
               />
             </div>
             <div style={{ flex: 1, marginLeft: "20px", paddingTop: "8px" }}>
               <MapContainer
                 center={[newStation.latitude, newStation.longitude]}
-                style={{ height: "630px", width: "100%" }}
+                style={{ height: "700px", width: "100%" }}
                 zoom={14}
               >
                 <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
